f0a8c9a3463cdb80fee3c2483953397acc62441e
Richard Liu <richard1421@msn.com>
2015-11-11 21:15:31 +1100

    Added slider updating on window resize

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
+var currentTab = null;
+
+
 $(function()
 {
     $('.tab').mouseenter(tabHover);
+    $(window).resize(windowResize);
 });
+
 
 function tabHover(e)
 {
     var slider = $('#slider');
-    var tab = $(e.target);
+    currentTab = $(e.target);
 
-    var x = tab.position().left + parseInt(tab.css('marginLeft').replace('px', ''));
     if (slider.css('display') == 'none')
     {
         slider.css('display', 'block');
-        slider.css('left', x);
+        slider.css('left', getTabX(currentTab));
+        slider.css('top', getTabY(currentTab));
     }
     else
     {
         slider.stop();
-        slider.animate({ 'left': x }, 200);
+        slider.animate({ 'left': getTabX(currentTab), 'top': getTabY(currentTab) }, 200);
     }
+}
+
+function windowResize(e)
+{
+    if (currentTab != null)
+    {
+        var slider = $('#slider');
+        slider.css('left', getTabX(currentTab));
+        slider.css('top', getTabY(currentTab));
+    }
+}
+
+function getTabX(tab)
+{
+    return currentTab.position().left + pxToInt(currentTab.css('marginLeft'));
+}
+function getTabY(tab)
+{
+    var slider = $('#slider');
+    return currentTab.position().top + pxToInt(currentTab.css('paddingTop')) + pxToInt(currentTab.css('paddingBottom')) +
+        pxToInt(currentTab.css('height')) - pxToInt(slider.css('height'));
+}
+
+function pxToInt(px)
+{
+    return parseInt(px.replace('px', ''));
 }
