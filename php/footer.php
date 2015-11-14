3acd0b4b856377e3c15e731fd642b31d420b5c94
Richard Liu <richard1421@msn.com>
2015-11-14 15:00:58 +1100

    Added year to copyright notice

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,8 @@
 <footer>
-    Copyright &copy; &lt;year&gt;
+    <?php
+        $start_year = 2015;
+        $cur_year = date('Y');
+        $str = $start_year.(($start_year != $cur_year) ? "-".$cur_year : "");
+        echo 'Copyright &copy; '.$str;
+    ?>
 </footer>
