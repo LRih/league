579fa2dc9db2b9891d005b2911b5881bb0248848
Richard Liu <richard1421@msn.com>
2015-11-14 11:30:42 +1100

    Updated boosting page

--- script/boosting.js
+++ script/boosting.js
@@ -4,74 +4,136 @@ $(function()
 {
     $(".dropdown-menu li a").click(function()
     {
+        var btn = getBtn($(this));
+
         // set selected text in button
-        var btn = $(this).parents(".dropdown").find('.btn');
         btn.html($(this).text() + ' <span class="caret"></span>');
+
+        // set button value
+        btn.val($(this).parent().index());
     });
 
-    // set button value
     $(".division-select .dropdown-menu li a").click(function()
     {
-        $(this).parents(".dropdown").find('.btn').val($(this).text());
+        // rename tiers according to division
+        var rankContainer = getRankContainer($(this));
+        var dropdown = rankContainer.find('.tier-select');
+        var tierList = dropdown.find('.dropdown-menu').children();
 
-        // rename tiers
-        var rankSelectContainer = getRankSelectContainer($(this));
-        var division = getDivision(rankSelectContainer);
+        var division = getDivisionName(rankContainer);
 
-        var tierList = rankSelectContainer.find('.tier-select .dropdown-menu').children();
         for (var i = 0; i < tierList.length; i++)
             tierList.eq(i).find('a').html(division + ' ' + TIERS[i]);
 
-        tierList.eq(5 - getTier(rankSelectContainer)).find('a').trigger('click');
-    });
-    $(".tier-select .dropdown-menu li a").click(function()
-    {
-        $(this).parents(".dropdown").find('.btn').val(5 - $(this).parent().index());
-    });
-    $(".region-select .dropdown-menu li a").click(function()
-    {
-        $(this).parents(".dropdown").find('.btn').val($(this).text());
+        if (isMastersChallenger(division))
+            dropdown.css('display', 'none');
+        else
+        {
+            dropdown.css('display', 'block');
+            updateDropdownText(dropdown);
+        }
     });
 
     // set rank image
     $(".division-select .dropdown-menu li a, .tier-select .dropdown-menu li a").click(function()
     {
-        var rankSelectContainer = getRankSelectContainer($(this));
-        var division = getDivision(rankSelectContainer);
-        var img = rankSelectContainer.find('.division-img');
+        var rankContainer = getRankContainer($(this));
+        var division = getDivisionName(rankContainer);
+        var img = rankContainer.find('.division-img');
 
-        if (division == 'Masters' || division == 'Challenger')
+        if (isMastersChallenger(division))
             img.attr('src', 'images/div_' + division + '.png');
         else
         {
-            var tier = rankSelectContainer.find('.tier-select > .btn').val();
+            var tier = 5 - getTierIndex(rankContainer);
             img.attr('src', 'images/div_' + division + '_' + tier + '.png');
         }
     });
 
-    // set default division to bronze
-    $(".dropdown-menu").each(function()
+    // update desired rank on current rank change
+    $("#current-rank .dropdown-menu li a").click(function()
+    {
+        var currentRank = getRankValue(getRankContainer($(this)));
+        var desiredRank = getRankValue($('#desired-rank'));
+
+        if (currentRank >= desiredRank)
+            setNextRankUp($('#desired-rank'), currentRank);
+    });
+
+    // set default list values
+    $('.dropdown-menu').each(function()
     {
         $(this).find('li a').first().trigger('click');
     });
+    $('#current-rank li').eq(0).find('a').trigger('click');
 });
 
-function getRankSelectContainer(child)
+
+function isMastersChallenger(division)
+{
+    return division == 'Masters' || division == 'Challenger';
+}
+
+
+function updateDropdownText(dropdown)
+{
+    var btn = dropdown.find('.btn');
+    var list = dropdown.find('.dropdown-menu').children();
+
+    btn.html(list.eq(btn.val()).find('a').html() + ' <span class="caret"></span>');
+}
+
+
+function getRankContainer(child)
 {
     return child.parents(".rank-select-container");
 }
 
-function getDivision(rankSelectContainer)
+function getBtn(child)
 {
-    return rankSelectContainer.find('.division-select > .btn').val();
+    return child.parents('.dropdown').find('.btn');
 }
 
-function getTier(rankSelectContainer)
+
+function getDivisionName(rankContainer)
 {
-    return rankSelectContainer.find('.tier-select > .btn').val();
+    var list = rankContainer.find('.division-select .dropdown-menu').children();
+    return list.eq(getDivisionIndex(rankContainer)).find('a').html();
+}
+function getDivisionIndex(rankContainer)
+{
+    return rankContainer.find('.division-select > .btn').val();
+}
+function getTierIndex(rankContainer)
+{
+    return rankContainer.find('.tier-select > .btn').val();
 }
 
-function getRankValue()
+
+function setNextRankUp(rankContainer, rank)
 {
-    return 0;
+    var divIndex, tierIndex;
+
+    if (rank == 500) // masters to challenger
+    {
+        divIndex = 6;
+        tierIndex = 0;
+    }
+    else if ((rank - 4) % 100 == 0) // from tier I
+    {
+        divIndex = Math.floor(rank / 100) + 1;
+        tierIndex = 0;
+    }
+    else
+    {
+        divIndex = Math.floor(rank / 100);
+        tierIndex = rank % 100 + 1;
+    }
+
+    rankContainer.find('.division-select .dropdown-menu').children().eq(divIndex).find('a').trigger('click');
+    rankContainer.find('.tier-select .dropdown-menu').children().eq(tierIndex).find('a').trigger('click');
+}
+function getRankValue(rankContainer)
+{
+    return parseInt(getDivisionIndex(rankContainer)) * 100 + parseInt(getTierIndex(rankContainer));
 }
